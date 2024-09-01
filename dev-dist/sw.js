45de3cc4cfaff3aa13c0db2e3e11747bb4d7facf
portellaluana <luanaportella@gmail.com>
2024-09-01 18:35:08 -0300

    Bolona: add screenshots

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ecd9dced'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kuodofithlo"
+    "revision": "0.dfiop6sjhp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
